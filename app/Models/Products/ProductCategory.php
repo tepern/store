717e0fdfa1e2fc0657cd7371a13164e59b9ff989
nteper <nteper@codemastersintl.com>
2025-05-23 15:39:40 +0300

    Category Model

---
+++ app/Models/Products/ProductCategory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Products;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProductCategory extends Model
+{
+    use HasFactory; use SoftDeletes;
+}
