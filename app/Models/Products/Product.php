1240dfa391a212e8892c2ed62cba7bbf868b31bb
nteper <nteper@codemastersintl.com>
2025-05-24 23:26:19 +0300

    Product model

---
+++ app/Models/Products/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\Products;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'category_id',
+        'price'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(ProductCategory::class);
+    }
+}
---
+++ database/migrations/2025_05_24_183547_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('category_id')->unsigned();
+            $table->text('description')->nullable();
+            $table->decimal('price', 13, 2);
+            $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('product_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
